66224d80c897019981a5ece5b58d18ccd4e72d91
jcjeancarlos <jcjeancarlos.1@gmail.com>
2026-02-19 15:06:37 +0100

    calendar full version
    Affiche le table de chaque mois précédé du nom du mois

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-let day = 1;
-let max_day = prompt('Combien de jours compte votre mois ?')
+let month_max_days = [
+    31, 28, 31, 30, 31, 30,
+    31, 31, 30, 31, 30, 31
+];
 
-max_day = parseInt(max_day)
+let month_names = [
+    "Janvier", "Février", "Mars", "Avril", "Mai", "Juin",
+    "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"
+];
 
 
-function calendar(max_day) {
-        document.write('<table>');
-        for (let line = 1; line <= 5; line++){
-            document.write('<tr>');
-            
-            for (let col = 1; col <= 7; col++){
-                if (day<=max_day) {
-                    
-                document.write('<td>'+day+'</td>');
-                ++day 
-                }  
+function print_table_month(month_max_days) {
+    document.write('<table>');
+    let day = 1;
+    for (let line = 1; line <= 5; line++) {
+        document.write('<tr>');
+
+        for (let col = 1; col <= 7; col++) {
+            if (day <= month_max_days) {
+
+                document.write('<td>' + day + '</td>');
+                ++day
             }
-
-            document.write('</tr>');
         }
-        document.write('</table>');
+
+        document.write('</tr>');
     }
+    document.write('</table>');
+}
+
+
+function calendar() {
+    for (let i = 0; i < 12; i++) {
+        document.write("<h3>" + month_names[i] + "</h3>");
+
+        print_table_month(month_max_days[i])
+    }
+}
